8d8c1f84dd83f265e290e227061b361a7279c8f8
Megh Shah <meghshah810@gmail.com>
2025-02-03 02:34:54 +0530

    App SetUp

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const PORT = 3000;
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  console.log("'/' requested!");
+  res.send("Hello World!");
+});
+
+app.listen(PORT, () => {
+  console.log(`App Listening at PORT ${PORT} `);
+});
